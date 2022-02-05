3c9a9bd47df39122ced5e9e422b438f4bbcd1833
DrPayne25 <alex.payne1125@gmail.com>
2022-02-05 14:24:17 -0800

    finished the count function working on Report Function

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -6,13 +6,18 @@ URL = 'https://en.wikipedia.org/wiki/Nintendo'
 def get_citations_needed_count(URL):
   URL = URL
   page = requests.get(URL)
-  pretty_page = BeautifulSoup(page.content, 'html.parser')
-  paragraph = pretty_page.find_all('p')
-  print(paragraph)
+  b_soup = BeautifulSoup(page.content, 'html.parser')
+  citations = b_soup.find_all(title="Wikipedia:Citation needed")
+  count = 0
+  for citation in citations:
+    count += 1
+  return(count)
   
 
-def get_citations_needed_report():
-  pass
+def get_citations_needed_report(URL):
+  URL = URL
+  page = requests.get(URL)
+  pretty_page = BeautifulSoup(page.content, 'html.parser')
 
 if __name__ == '__main__':
   count = get_citations_needed_count(URL)
